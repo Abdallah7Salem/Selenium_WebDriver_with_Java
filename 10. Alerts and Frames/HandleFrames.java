ca8bdf8497ff0d814e28d86a1768f94cbe9c25a8
Abdallah Salem <57125259+Abdallah7Salem@users.noreply.github.com>
2025-02-22 07:08:32 +0200

    Practice handling frames using Selenium

---
+++ 10. Alerts and Frames/HandleFrames.java
@@ -0,0 +1,57 @@
+package day30;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HandleFrames {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+		driver.get("https://ui.vision/demo/webtest/frames/");
+		driver.manage().window().maximize();
+		
+		// Frame 1
+		WebElement frame1 = driver.findElement(By.xpath("//frame[@src = 'frame_1.html']"));
+		driver.switchTo().frame(frame1);
+		driver.findElement(By.xpath("//input[@name='mytext1']")).sendKeys("Welcome");
+		
+		// Switch to the main page
+		driver.switchTo().defaultContent();
+		
+		// Frame 2
+		WebElement frame2 = driver.findElement(By.xpath("//frame[@src = 'frame_2.html']"));
+		driver.switchTo().frame(frame2);
+		driver.findElement(By.xpath("//input[@name = 'mytext2']")).sendKeys("Automation");
+		
+		// Switch to the main page
+		driver.switchTo().defaultContent();		
+		
+		// Frame 3 
+		WebElement frame3 = driver.findElement(By.xpath("//frame[@src = 'frame_3.html']"));
+		driver.switchTo().frame(frame3);
+		driver.findElement(By.xpath("//input[@name = 'mytext3']")).sendKeys("programming");
+		
+		// Switch to inner frame (part of frame 3)
+		driver.switchTo().frame(0);
+		
+		// Select a radio button in the iframe
+		driver.findElement(By.xpath("//div[@id='i6']//div[@class='AB7Lab Id5V1']")).click();
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
