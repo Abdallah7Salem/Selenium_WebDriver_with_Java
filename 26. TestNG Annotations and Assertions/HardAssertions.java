e1e98c36b445cf83cf4ae3d59eca139b612ad363
Abdallah Salem <57125259+Abdallah7Salem@users.noreply.github.com>
2025-03-02 09:10:45 +0200

    Practicing hard assertions in TestNG

---
+++ 26. TestNG Annotations and Assertions/HardAssertions.java
@@ -0,0 +1,46 @@
+package day43;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class HardAssertions {
+
+	@Test
+	void test()
+	{
+		// Assert.assertEquals("xyz", "xyz");
+		// Assert.assertEquals(123, 456);
+	
+		// Assert.assertEquals("abc", 123);
+		// Assert.assertEquals("123", 123);
+	
+		// Assert.assertNotEquals(123, 123);
+		// Assert.assertNotEquals(123, 456);
+		
+		// Assert.assertTrue(true);
+		// Assert.assertTrue(false);
+		
+		// Assert.assertTrue(1 == 2);
+		// Assert.assertTrue(1 == 1);
+		
+		// Assert.assertFalse(1 == 2);
+		// Assert.assertFalse(1 == 1);
+		
+		Assert.fail();
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
